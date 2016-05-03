76b1504a176fcc512e2e5a1b310dc3b9b79f4241
Ashley Maceli and Elissa Sosland <ashley@>
2016-05-03 09:41:35 -0700

    Add updated passing integration tests and new GET route for category IDs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -46,6 +46,14 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("categories/:id/tasks/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.params(":id")));
